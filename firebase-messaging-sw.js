c0ef2bcc2874b67f784889b97ce0944c2c66dd5f
Hidey Boi <maxg2070319@gmail.com>
2022-04-13 14:30:01 -0500

    hopefully it's mfing fixed lmao

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-import firebase from 'firebase';
-import 'firebase/messaging';
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js')
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js')
 
 if (!firebase.apps.length) {
     firebase.initializeApp({
