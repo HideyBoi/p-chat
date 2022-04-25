3d45665b7d5fc95c162db5ee58ddecfba4a03971
Hidey Boi <maxg2070319@gmail.com>
2022-04-25 09:27:14 -0500

    Revert "Various fixes + smart timestamps"
    This reverts commit ea69f74f36c88c5ef53ca322935e08c97bbd4208.

--- precache-manifest.ddb949624f40c8231321927131958c9f.js
+++ precache-manifest.ddb949624f40c8231321927131958c9f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "57f9aecb365d6046af3dcdea92d827c3",
+    "revision": "d1590f781719d16c17eb7e1ee9cb745c",
     "url": "/p-chat/index.html"
   },
   {
-    "revision": "54f1d8db141dc7cb1e52",
-    "url": "/p-chat/static/css/main.dbbc0e7a.chunk.css"
+    "revision": "c735552b0aa275587f6c",
+    "url": "/p-chat/static/css/main.9586789a.chunk.css"
   },
   {
-    "revision": "37c6d5b290780bdc1082",
-    "url": "/p-chat/static/js/2.8d3b27fa.chunk.js"
+    "revision": "eb2bf89c7ba5f5201149",
+    "url": "/p-chat/static/js/2.4c3dc303.chunk.js"
   },
   {
     "revision": "f62a3bc48fdf8cb6c92c54737bdc6961",
-    "url": "/p-chat/static/js/2.8d3b27fa.chunk.js.LICENSE.txt"
+    "url": "/p-chat/static/js/2.4c3dc303.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "54f1d8db141dc7cb1e52",
-    "url": "/p-chat/static/js/main.a164f43e.chunk.js"
+    "revision": "c735552b0aa275587f6c",
+    "url": "/p-chat/static/js/main.9a21ac7f.chunk.js"
   },
   {
     "revision": "9ef07cd64f3e6eb464e2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/p-chat/precache-manifest.dd46f93e55a3355a049dd42d59a30d1d.js"
+  "/p-chat/precache-manifest.ddb949624f40c8231321927131958c9f.js"
 );
 
 self.addEventListener('message', (event) => {
